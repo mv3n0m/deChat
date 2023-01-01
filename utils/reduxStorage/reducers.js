37d94cda80c2121303b01ee3309b16b88a88771b
Manish Roy <manish.11612939@lpu.in>
2023-01-02 00:00:32 +0530

    updated reducer for avatar and chatroom selection

--- utils/reduxStorage/reducers.js
+++ utils/reduxStorage/reducers.js
@@ -2,12 +2,18 @@ import { retrieveLocal, storeLocal, deleteLocal } from "../sessionStorage"
 
 
 const usersReducer = ( state, action ) => {
-    const { account } =  action
+    const { account, avatarStyle, chatroom } =  action
 
     switch (action.type) {
         case 'USER_AUTHENTICATED':
             account ? storeLocal("isAuthenticated", account) : deleteLocal("isAuthenticated")
             return { ...state, account: retrieveLocal("isAuthenticated") }
+
+        case 'AVATAR_UPDATED':
+            return { ...state, avatarStyle }
+
+        case 'CHATROOM_SELECTED':
+            return { ...state, chatroom }
 
         default:
             return state || null
