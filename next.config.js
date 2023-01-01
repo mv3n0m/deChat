0d756e15c8981495f344bd7ca276bc0f8bcb9aee
Manish Roy <manish.11612939@lpu.in>
2023-01-01 18:30:37 +0530

    included dicebear domain for avatars

--- next.config.js
+++ next.config.js
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
-    domains: ["stock-images-mv3n0m.s3.amazonaws.com"]
+    domains: ["stock-images-mv3n0m.s3.amazonaws.com", "api.dicebear.com"]
   }
 }
 
