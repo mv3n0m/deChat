dc2c25c99f7b9f4c2a6a14ca1f0a276b9edc53ce
Manish Roy <manish.11612939@lpu.in>
2023-01-02 00:05:44 +0530

    updated Home component for new changes

--- pages/Home.tsx
+++ pages/Home.tsx
@@ -1,13 +1,21 @@
-import { Header } from "@/components";
+import { Header, Avatar } from "@/components";
+import { useSelector } from "react-redux";
 
 
-function User() {
+function Home() {
+    const avatarStyle = useSelector((state: any) => state.usersReducer?.avatarStyle) || "avataaars"
+    const chatroom = useSelector((state: any) => state.usersReducer?.chatroom) || "general"
+    const address = useSelector((state: any) => state.usersReducer?.account)
+
+    const avatar = <Avatar text={ address } avatarStyle={ avatarStyle }/>
 
     return (
-        <div className="h-screen overflow-y-scroll bg-gradient-to-b from-black to-fuchsia-500 ">
-            <Header />
+        <div className="h-screen bg-gradient-to-b from-black to-fuchsia-500 ">
+            <Header chatroom={ chatroom }>
+                <div className="w-20 h-20 relative border-2 border-fuchsia-500 shadow-lg rounded-full">{ avatar }</div>
+            </Header>
         </div>
     );
 }
 
-export default User;
+export default Home;
