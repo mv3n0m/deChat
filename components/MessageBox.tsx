2ba811d11c26b49e48ff3498f148c378990e1a51
Manish Roy <manish.11612939@lpu.in>
2023-02-03 00:03:07 +0530

    renamed Message component to MessageBox and updated features

---
+++ components/MessageBox.tsx
@@ -0,0 +1,42 @@
+import Image from "next/image"
+import { useRef, useState } from "react"
+import { useSelector } from "react-redux"
+
+
+function MessageBox({ chatroom, username }: { chatroom: string, username: string }) {
+    const avatarStyle = useSelector((state: any) => state.usersReducer?.avatarStyle) || "bottts"
+    const inputRef = useRef<HTMLInputElement>(null)
+    const [message, setMessage] = useState("")
+
+    const handleSubmit = async (event: any) => {
+        event.preventDefault()
+        inputRef.current?.focus()
+        if (!message?.length) return
+
+        const { status } = await fetch(process.env.NEXT_PUBLIC_CHAT_BACKEND + "/message", {
+            method: "POST",
+            body: new URLSearchParams({ room: chatroom, username, message, avatar_style: avatarStyle })
+        })
+
+        if (status !== 200) return alert("Unable to message.")
+        setMessage("")
+    }
+
+    return (
+        <div className="mx-auto w-screen lg:w-3/5 px-10 mb-28 sm:mb-5">
+            <form className="flex w-full bg-black rounded-full shadow-lg" onSubmit={handleSubmit}>
+                <input placeholder="Type here..." ref={inputRef} value={message} onChange={e => setMessage(e.target.value)}
+                    className="grow rounded-full bg-black placeholder:text-zinc-50 font-semibold px-5 py-3 focus:outline-none text-white"
+                />
+                <button type="submit" className="bg-white rounded-full p-3 border-2 border-fuchsia-500">
+                    <Image alt="send"
+                        src="/send.png"
+                        height={20} width={20}
+                    />
+                </button>
+            </form>
+        </div>
+    )
+}
+
+export default MessageBox
