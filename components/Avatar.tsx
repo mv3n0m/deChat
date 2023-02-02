f75f98621f1790c3c26203c642de2f2f8d64320e
Manish Roy <manish.11612939@lpu.in>
2023-02-03 00:04:15 +0530

    updated Avatar component for defaults

--- components/Avatar.tsx
+++ components/Avatar.tsx
@@ -1,3 +1,4 @@
+import { useSession } from "next-auth/react"
 import Image from "next/image"
 
 
@@ -10,12 +11,14 @@ type avatarProps = {
 }
 
 function Avatar({ text, size, className, avatarStyle, onClick }: avatarProps) {
+  const { data: session } = useSession()
   const props = size ? { width: size, height: size } : { fill: true }
+  const avatarSeed = text === "anon" ? (session?.user as any)?.id : text
 
   return (
     <Image alt="arrow"
       { ...props }
-      src={`https://api.dicebear.com/6.x/${avatarStyle}/png?seed=${text}`}
+      src={`https://api.dicebear.com/6.x/${avatarStyle || "thumbs"}/png?seed=${avatarSeed}`}
       className={`rounded-full inline-block p-1 ${className}`}
       onClick={onClick}
     />
