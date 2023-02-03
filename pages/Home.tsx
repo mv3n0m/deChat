67af53a0b565b29d0fa2f7d83a5ec6162f4f958f
Manish Roy <manish.11612939@lpu.in>
2023-02-03 15:35:01 +0530

    updated Home component for login check, avatar integration and Chats component

--- pages/Home.tsx
+++ pages/Home.tsx
@@ -1,20 +1,44 @@
-import { Header, Avatar } from "@/components";
-import { useSelector } from "react-redux";
+import { Chats, Header, Avatar } from "@/components";
+import { retrieveLocal } from "@/utils";
+import { useEffect, useState } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import Login from "./Login";
 
 
 function Home() {
+    const dispatch = useDispatch()
     const avatarStyle = useSelector((state: any) => state.usersReducer?.avatarStyle) || "avataaars"
     const chatroom = useSelector((state: any) => state.usersReducer?.chatroom) || "general"
     const address = useSelector((state: any) => state.usersReducer?.account)
+    const [authenticated, isAuthenticated] = useState(true)
 
-    const avatar = <Avatar text={ address } avatarStyle={ avatarStyle }/>
+    const components = [
+        { Parent: Header, size: 20, className: "border-2 border-fuchsia-500" },
+        { Parent: Chats, size: 12 }
+    ]
+
+    useEffect(() => {
+        const account = retrieveLocal("isAuthenticated")
+        dispatch({ type: 'USER_AUTHENTICATED', account })
+        isAuthenticated(account || address)
+    }, [ address ])
+
+    const createComponentWithAvatar = ({ Parent, size, className }: { Parent: any, size: number, className?: string }) => (
+        <Parent chatroom={chatroom} key={ size }>
+            <div className={`w-${size} h-${size} bg-black relative shadow-lg rounded-full ${className}`}>
+                <Avatar text={address} avatarStyle={avatarStyle} />
+            </div>
+        </Parent>
+    )
 
     return (
-        <div className="h-screen bg-gradient-to-b from-black to-fuchsia-500 ">
-            <Header chatroom={ chatroom }>
-                <div className="w-20 h-20 relative border-2 border-fuchsia-500 shadow-lg rounded-full">{ avatar }</div>
-            </Header>
-        </div>
+        <>{
+            authenticated ? (
+                <div className="h-screen bg-gradient-to-b from-black to-fuchsia-500 ">
+                    {components.map(component => createComponentWithAvatar(component))}
+                </div>
+            ) : ( <Login /> )
+        }</>
     );
 }
 
