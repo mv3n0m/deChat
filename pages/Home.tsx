6993b04ccd29abd6b1fef4bcb01ae246f174ca90
Manish Roy <manish.11612939@lpu.in>
2023-02-25 00:02:21 +0530

    optimised Home component for latest updates

--- pages/Home.tsx
+++ pages/Home.tsx
@@ -1,44 +1,29 @@
-import { Chats, Header, Avatar } from "@/components";
+import { Chats, Header, Avatar, MessageBox } from "@/components";
 import { retrieveLocal } from "@/utils";
 import { useEffect, useState } from "react";
-import { useDispatch, useSelector } from "react-redux";
-import Login from "./Login";
+import { useSelector } from "react-redux";
 
 
 function Home() {
-    const dispatch = useDispatch()
-    const avatarStyle = useSelector((state: any) => state.usersReducer?.avatarStyle) || "avataaars"
-    const chatroom = useSelector((state: any) => state.usersReducer?.chatroom) || "general"
-    const address = useSelector((state: any) => state.usersReducer?.account)
-    const [authenticated, isAuthenticated] = useState(true)
+    const _chatroom = useSelector((state: any) => state.usersReducer?.chatroom)
+    const _username = useSelector((state: any) => state.usersReducer?.username)
 
-    const components = [
-        { Parent: Header, size: 20, className: "border-2 border-fuchsia-500" },
-        { Parent: Chats, size: 12 }
-    ]
+    const [ chatroom, setChatroom ] = useState("")
+    const [ username, setUsername ] = useState("")
 
     useEffect(() => {
-        const account = retrieveLocal("isAuthenticated")
-        dispatch({ type: 'USER_AUTHENTICATED', account })
-        isAuthenticated(account || address)
-    }, [ address ])
-
-    const createComponentWithAvatar = ({ Parent, size, className }: { Parent: any, size: number, className?: string }) => (
-        <Parent chatroom={chatroom} key={ size }>
-            <div className={`w-${size} h-${size} bg-black relative shadow-lg rounded-full ${className}`}>
-                <Avatar text={address} avatarStyle={avatarStyle} />
-            </div>
-        </Parent>
-    )
+        setChatroom(_chatroom || retrieveLocal("chatroom", "general"))
+        setUsername(_username || retrieveLocal("username", "anon"))
+    }, [ _username, _chatroom ])
 
     return (
-        <>{
-            authenticated ? (
-                <div className="h-screen bg-gradient-to-b from-black to-fuchsia-500 ">
-                    {components.map(component => createComponentWithAvatar(component))}
-                </div>
-            ) : ( <Login /> )
-        }</>
+        <div className="h-screen bg-gradient-to-b from-black to-fuchsia-500 flex flex-col">
+            {
+                [Header, Chats, MessageBox].map((Component, idx) => (
+                    <Component chatroom={chatroom} username={username} key={idx} />
+                ))
+            }
+        </div>
     );
 }
 
